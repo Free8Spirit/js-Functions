50f4b56c5fc0a5e927386a3ce7f4ac0c5badeffb
Free8Spirit <davidvert2016@gmail.com>
2018-09-05 10:03:57 -0500

    min of 2 nums complete

---
+++ minOf2Nums.js
@@ -0,0 +1,18 @@
+//Modified by Vert, David
+
+function min(x, y) {
+    if (x > y) {
+        return y;
+    } else if (x < y) {
+        return x;
+    } else if (x = y) {
+        return x || y;
+    } else {
+        return "nanananana";
+    }
+}
+
+console.log(min(4.5, 5)); // Must show 4.5
+console.log(min(19, 9));  // Must show 9
+console.log(min(1, 1));   // Must show 1
+console.log(min(8, 8));   // Must show 8
